b048d00873e03f9ed341c7bfc7903e85cdf2e971
Sangyal88 <71094878+Sangyal88@users.noreply.github.com>
2021-07-19 20:18:38 +0530

    Update clustering.m

--- clustering.m
+++ clustering.m
@@ -13,6 +13,16 @@ A=int16(im1);
 %A=double(im1);
 m=zeros(row,col);
 min=double(m);
+
+k=2;%No. of Clusters
+c1=zeros(row,col);%cluster 1
+c2=zeros(row,col);%cluster 2
+c1=int16(c1);
+c2=int16(c2);
+n=numel(A);%total elements in A
+t=0.75*(n/k);%75 percent
+[c1,c2] = Pdistance2(X, c1, c2, k, Ck, ind_i, ind_j, t, min );
+
 [mindist, mindistvalue, index_i, index_j, mid]= pointdistance(A, 1, 2, Ck);
 fprintf('\n');
 fprintf('MinVal=%i', mindistvalue);
