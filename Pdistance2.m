cfeaf1b456af529360449fcf8adc58b64ab2d59f
Sangyal88 <71094878+Sangyal88@users.noreply.github.com>
2021-07-19 21:04:38 +0530

    Update Pdistance2.m

--- Pdistance2.m
+++ Pdistance2.m
@@ -1,12 +1,10 @@
-function [ c1,c2] = Pdistance2(X, c1, c2, k, Ck, ind_i, ind_j, t, min )
+function [ c1,c2] = Pdistance2(X, c1, c2, k, Ck, ind_i, ind_j, t, min)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
  [row,col] = size(X);
     minVal=int16(min); 
     mindistvalue = 9999; % some arbitrary large value
-                     fprintf('\n');
-                     fprintf('Initial Mindistance=%i', mindistvalue);
-                     fprintf('\n');
+                    
             for i = 1:row
                 for j = 1 :col
                     val = X(i,j);
@@ -20,7 +18,7 @@ function [ c1,c2] = Pdistance2(X, c1, c2, k, Ck, ind_i, ind_j, t, min )
                                 mindistvalue = minVal(i,j);
                                %index_i = i;
                                 %index_j = j;
-                                if (
+                                if (numel(c1)<t)
 
                             end
                         end
