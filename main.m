87ecc6f31caa6a1b178a8732cb450b2a7163be8f
rajgurung777 <rajgurung777@gmail.com>
2021-07-28 20:11:05 +0530

    Update main.m
    Made a few online changes

--- main.m
+++ main.m
@@ -8,20 +8,17 @@ Ck = 999;
 A=int16(im1); %A=double(im1);
 A1=A; %keep a copy of A
 n = numel(A); %total elements in A
-countelement=0;
+
 nk=input('Enter the number of cluster : ');
 t = 0.5*(n/nk); %75 percent
 fprintf('\n')
 [row,col]=size(im1);
 for z=1:nk
-    r(z)=0;
-    k(z)=0;
-    n(z)=0;
-    fk(z)=0;
-    Cs(z).C =zeros(row,col);    
+    Cs(z).C =zeros(row,col);   
+    Cs(z).centroid =0; 
 end
 tic
 %[ c1,c2 ] = cluster( Ck,A1,c1,c2,t,countelement );
 %grayIm1(nk,k,Cs,n,fk,A1);
-[ Cs, A1 ] = cluster2( Ck,nk,A1,Cs,t,countelement );
+[ Cs, A1 ] = cluster2( Ck,nk,A1,Cs,t);
 toc
