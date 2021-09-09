414e3f1109f45c0b78c239f87b61306e9940c506
Sangyal88 <71094878+Sangyal88@users.noreply.github.com>
2021-09-10 00:49:55 +0530

    Update pointdistance.m

--- Test2/pointdistance.m
+++ Test2/pointdistance.m
@@ -1,23 +1,14 @@
 function [ temp, h, l ] = pointdistance(X)
-%function [mindistvalue, index_i, index_j] = pointdistance(X, ind_i, ind_j, Ck)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    [row, col] = size(X);
-    temp= X(row,col);
-%minVal=zeros(row, col)+Ck;% Initialize constant value 999 to all elements
-%mindistvalue = 9999; % some arbitrary large value
+[row, col] = size(X);
+temp= X(row,col);
 for i = 1:row
     for j = 1 : col
         val = X(i,j);
-        %if (Ck ~= val) % only then do the comparision
-            %if ind_i == i  && ind_j == j
-                %continue %skip this particular element
-            %else % for all other element; process them
-                diffValue = abs(val - X(1, 1));
-                temp(i,j) = diffValue;
-           % end
-        %end
-    end
+        diffValue = abs(val - X(1, 1));
+        temp(i,j) = diffValue;
+     end
 end
 high = max(temp);
 h = max(high);
