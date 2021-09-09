8b576dc14b08fd91c0d1ee22cfb7ea7656cfc8ab
Sangyal88 <71094878+Sangyal88@users.noreply.github.com>
2021-09-10 00:52:45 +0530

    Update cluster_2.m

--- Test2/cluster_2.m
+++ Test2/cluster_2.m
@@ -17,39 +17,21 @@ while(n<h+1)
                 if(count<5)
                     c(k).clus(i,j)= A1(i,j);
                     temp(i,j)=999;
-                    %count = count+1;
+                    
                 else
                     k=k+1;
                     c(k).clus(i,j)=A1(i,j);
                     temp(i,j)=999;
                     count=0;
-                    %fprintf('k+1');
-                    %fprintf('\n');
-                    %fprintf('The value of k is %i',k);
-                    %fprintf('\n');
+                   
                 end
-                %fprintf('1st point');
-                %fprintf('\n');
-                %if(count==5)
-                   % count = 0;
-                    %k = k+1;
-                %end
-               % fprintf('%i',k);
-                % fprintf('\n');
-               % fprintf('2nd point');
-                %fprintf('\n');
+                
            end
         end
     end
     count=count+1;
     n=n+1;
-    %fprintf('\n');
-    %fprintf('%i',count);
-    %fprintf('\n');
-    %fprintf('%i',n);
-    %fprintf('\n');
-    %fprintf('%i',k);
-    %fprintf('\n');
+    
 end
  fprintf('The value of k is %i',k);
  fprintf('\n');
