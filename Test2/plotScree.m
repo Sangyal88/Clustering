f9d9f2ada3efb79e4b69c9dddd4943a68261a8c3
Sangyal88 <71094878+Sangyal88@users.noreply.github.com>
2021-10-22 00:17:19 +0530

    Add files via upload

---
+++ Test2/plotScree.m
@@ -0,0 +1,18 @@
+function wss = plotScree(X1, n)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+X=double(X1);
+wss = zeros(1, n);
+wss(1) = (size(X, 1)-1) * sum(var(X, [], 1));
+for i=2:n
+    T = clusterdata(X,'maxclust',i);
+    wss(i) = sum((grpstats(T, T, 'numel')-1) .* sum(grpstats(X, T, 'var'), 2));
+end
+hold on
+plot(wss)
+plot(wss, '.')
+xlabel('Number of clusters')
+ylabel('Within-cluster sum-of-squares')
+
+end
+
