03428fc4bb035cace5ced32549a2eba9b6be9038
rajgurung777@gmail.com <rajgurung777@gmail.com>
2022-01-16 20:31:54 +0530

    Fixed plotting for Elbow method

--- Test2 - Modified/main.m
+++ Test2 - Modified/main.m
@@ -13,7 +13,7 @@ A1=A; %keep a copy of A
 se=0;
 
 
-tic
+% tic
 %[ c1,c2 ] = cluster( Ck,A1,c1,c2,t,countelement );
 %grayIm1(nk,k,Cs,n,fk,A1);
 %[ Cs, A1 ] = cluster2( Ck,nk,A1,Cs,t);
@@ -21,25 +21,31 @@ tic
 %[IDX,C,SUMD,Kb]=best_kmeans(A1);
 m = input('Enter the number of cluster : ');
 s=zeros(m-1,2);
-s=int32(s); 
+% s=int32(s); 
 a=1;
-%%%%%for i= 2:m
-[ temp, h, l ] = pointdistance(A1);
 
-% [ c, temp1, k, cen ] = cluster_2( l, h, temp, A1, i);
-[ c, temp1, cen ] = cluster_22(l, h, temp, A1, m); %%When the initial value of c.clus is 555
+for i= 2:m
+    tic
+    [ temp, h, l ] = pointdistance(A1);
+    [ c, temp1, cen ] = cluster_22(l, h, temp, A1, i); %%When the initial value of c.clus is 555
+    toc
 %plotScree(A1, nk, h, l, temp);
 %[sse1,A1]=SSE(nk,c,A1);
 %for i=2:m
-% % % [sse1, im1]=SSE(i,c,cen,A1);
-% % % s(a,1)= i;
-% % % s(a,2)=sse1;
-% % % a=a+1;
-%%%%end
+    [sse1, im1]=SSE(i,c,cen,A1);
+    s(i-1,1)= i;
+    s(i-1,2)= sse1;
+%     a=a+1;
+end
 hold on
-plot(s)
-plot(s, '.')
+
+x = s( :, 1);
+y = s(:, 2);
+
+%plot(s)
+plot(x, y)
+plot(x, y, 'g*')
 xlabel('Number of clusters')
 ylabel('Within-cluster sum-of-squares')
-toc
+% toc
 [f1,A1] = disp1(m,c,cen,A1);
